e588cc3b8318534136aaac9d1b67428c8691645c
KevinMoehring <35976299+IT1050-KevinMoehring@users.noreply.github.com>
2019-09-15 19:38:03 -0400

    Add files via upload

---
+++ Lab1Part3d.sql
@@ -0,0 +1,9 @@
+/*
+*********************************************************************
+Downloaded from GeeksEngine.com
+edited by Kevin Moehring, 9/15/19
+*********************************************************************
+*/
+
+Select concat(LastName, ", ", Firstname) From Employees
+where LastName between "H" and "W";
