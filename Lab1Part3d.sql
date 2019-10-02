219b0ff42092c45319c72c3cf9b24cd4c0399a88
KevinMoehring <35976299+IT1050-KevinMoehring@users.noreply.github.com>
2019-10-02 06:36:58 -0400

    Delete Lab1Part3d.sql

--- Lab1Part3d.sql
+++
@@ -1,9 +0,0 @@
-/*
-*********************************************************************
-Downloaded from GeeksEngine.com
-edited by Kevin Moehring, 9/15/19
-*********************************************************************
-*/
-
-Select concat(LastName, ", ", Firstname) From Employees
-where LastName between "H" and "W";
